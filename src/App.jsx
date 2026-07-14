a51c86ca2a74259ccbef917ffe65ed496582c644
ahmed10mohsen74 <106989484+ahmed10mohsen74@users.noreply.github.com>
2026-07-14 14:13:56 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
-    <div> 
-      <h1 className='text-3xl text-red-500'>ahmed mohsen</h1>
+    <div className="text-3xl">
+      App
+      <div>
+        <h1 className="text-3xl text-red-500">ahmed mohsen</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
